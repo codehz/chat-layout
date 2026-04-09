9d1f9621e2fda8866eb7206f5d271b1f2fafb822
codehz <git@hertz.moe>
2026-04-10 00:24:45 +0800

    feat(nodes): export shrinkwrap node

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -2,4 +2,5 @@ export { Group, Wrapper } from "./base";
 export { Fixed, PaddingBox } from "./box";
 export { Flex, FlexItem } from "./flex";
 export { Place } from "./place";
+export { ShrinkWrap } from "./shrinkwrap";
 export { MultilineText, Text } from "./text";
